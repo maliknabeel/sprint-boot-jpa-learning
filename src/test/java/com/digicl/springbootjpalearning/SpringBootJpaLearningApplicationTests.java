a5823bc15e40c2156f05a8520259c0313233ced3
maliknabeel <nabeel.ahmed@seecs.edu.pk>
2023-05-31 04:38:48 +0500

    code structure

---
+++ src/test/java/com/digicl/springbootjpalearning/SpringBootJpaLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digicl.springbootjpalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
